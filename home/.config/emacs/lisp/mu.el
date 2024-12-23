b33a9625e67a945361761b83cc68dac4ba05047e
Ian S. Pringle <ian@dapringles.com>
2024-12-23 10:46:50 -0600

    updated mu4e

--- home/.config/emacs/lisp/mu.el
+++ home/.config/emacs/lisp/mu.el
@@ -21,19 +21,36 @@
   (mu4e-change-filenames-when-move t)
   (mu4e-update-interval (* 10 60))
   (mu4e-compose-format-flowed t)
+
   (mu4e-get-mail-command "mbsync -Va")
+
   (mu4e-maildir "~/mail")
   (mu4e-drafts-folder "/Drafts")
   (mu4e-sent-folder "/Sent")
-  (mu4e-refile-folder "/All Mail")
+  (mu4e-refile-folder "/Archive")
   (mu4e-trash-folder "/Trash")
-  (mu4e-maildir-shortcuts '(("/inbox" . ?i)
+  
+  (mu4e-maildir-shortcuts '(("/INBOX" . ?i)
                             ("/Sent" . ?s)
                             ("/Trash" . ?t)
                             ("/Drafts" . ?d)
-                            ("/All Mail" . ?a)))
-  (message-send-mail-function 'smtpmail-send-it
-                              auth-sources '("~/.authinfo")
-                              smtpmail-smtp-server "127.0.0.1"
-                              smtpmail-smtp-service 1025
-                              smtpmail-stream-type 'ssl))
+                            ("/Archive" . ?a)))
+  
+  (message-send-mail-function 'smtpmail-send-it)
+  (smtpmail-auth-credentials "~/.authinfo")
+  (smtpmail-smtp-server "127.0.0.1")
+  (smtpmail-smtp-service 1025)
+  (smtpmail-stream-type 'starttls))
+
+(use-package org-msg
+  :after mu4e
+  :custom
+  (mail-user-agent 'mu4e-user-agent)
+  (org-msg-options "html-postamble:nil H:5 num:nil ^:{} toc:nil authot:nil email:nil \\n:t")
+  (org-msg-startup "hidestars indent inlineimages")
+  (org-msg-default-alernatives '((new . (text html))
+                                 (reply-to-html . (text html))
+                                 (reply-to-text . (text))))
+  (org-msg-convert-citation t)
+  :config
+  (org-msg-mode))
