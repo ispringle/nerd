377adb6ad44b4c577e27d03bd463ebddd06c946f
Ian S. Pringle <ian@dapringles.com>
2025-01-17 13:08:29 -0600

    updates

--- home/.config/emacs/lisp/gptel.el
+++ home/.config/emacs/lisp/gptel.el
@@ -1,9 +1,12 @@
 (use-package gptel
-  :custom
-  (gptel-default-mode 'org-mode)
-  (gptel-model 'claude-3-sonnet-20240229)
-  (gptel-backend (gptel-make-anthropic "Claude"
-                   :stream t
-                   :key (isp/read-file-contents "~/.config/secrets/anthropic-console"))))
+  :defer t
+  :commands (gptel gptel-menu gptel-send gptel-request)
+  :config
+  (setq gptel-default-mode 'org-mode)
+  (setq gptel-model 'claude-3-sonnet-20240229)
+  (setq gptel-backend (gptel-make-anthropic "Claude"
+                        :stream t
+                        :key (isp/read-file-contents "~/.config/secrets/anthropic-console"))))
 
-(use-package elysium)
+(use-package elysium
+  :after 'gptel)
